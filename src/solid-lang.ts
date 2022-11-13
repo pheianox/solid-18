193c02ca14a9ed2f3c13bb70a79c778b03c7b66f
pheianox <pheianox@gmail.com>
2022-11-13 19:35:19 +0400

    build: Init
    Init

---
+++ src/solid-lang.ts
@@ -0,0 +1,46 @@
+import { createSignal, createMemo } from "solid-js"
+
+type Params = Record<string, any>
+type Translation = Record<string, string | ((params: Params) => string)>
+type Dictinary = Record<string, Translation>
+
+type Prev = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+  11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...0[]]
+
+type Path<T, D extends number = 10> = [D] extends [never] ? never : T extends object ?
+  { [K in keyof T]-?: [K] | (Path<T[K], Prev[D]> extends infer P ?
+    P extends [] ? never : Cons<K, P> : never
+  ) }[keyof T]
+  : [];
+
+type Cons<H, T> = T extends readonly any[] ?
+  ((h: H, ...t: T) => void) extends ((...r: infer R) => void) ? R : never
+  : never;
+
+
+export const createI18nContext = <T extends Dictinary>(dictinary_: T, language_: keyof T) => {
+  const [language, setLanguage] = createSignal(language_)
+  const [dictinary, setDictinary] = createSignal(dictinary_)
+  const languages = () => Object.keys(dictinary())
+  const translations = createMemo(() => dictinary()[language()])
+
+  function lookup(object: Record<any, any>, path: string[], defaultValue: any) {
+    const value = path.reduce((obj, key) => obj ? obj[key] : undefined, object)
+    return value === undefined ? defaultValue : value
+  }
+
+  function substitute(translation: string, params: Params) {
+    return translation.replace(/{{(.*?)}}/g, (_, param) => params[param])
+  }
+
+  function translate(path: Path<T[keyof T]>, params?: Params) {
+    const value = lookup(translations(), path as string[], "")
+    switch (typeof value) {
+      case "function": return value(params)
+      case "string": return params ? substitute(value, params) : value
+      default: return value
+    }
+  }
+
+  return { translate, language, languages, setLanguage, dictinary, setDictinary }
+}
---
+++ test/solid-lang.test.ts
@@ -0,0 +1,26 @@
+// import { createI18nContext } from "../src/solid-lang"
+
+// describe("Test", () => {
+//   const dictinary = {
+//     az: {
+//       title: "Xoş gəlmisiniz!",
+//     },
+//     ru: {
+//       title: 'Добро пожаловать!'
+//     },
+//     en: {
+//       title: 'Welcome!'
+//     }
+//   }
+
+//   const { translate, setLanguage } = createI18nContext(dictinary, "az")
+
+//   it("switches languages", () => {
+//     setLanguage("az")
+//     expect(translate(["title"])).toBe(dictinary.az.title)
+//     setLanguage("ru")
+//     expect(translate(["title"])).toBe(dictinary.ru.title)
+//     setLanguage("en")
+//     expect(translate(["title"])).toBe(dictinary.en.title)
+//   })
+// })
